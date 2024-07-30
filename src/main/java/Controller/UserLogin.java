6eff6b8282470fedb3e2503dc0cf9af1978808cf
yenvi12 <huynhthaovi27@gmail.com>
2024-07-30 22:31:10 +0700

    first commit

---
+++ src/main/java/Controller/UserLogin.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Database.DatabaseConnection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet("/UserLogin")
+public class UserLogin extends HttpServlet {
+
+    private String user;
+    private String pass;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        try {
+            String userp = request.getParameter("username");
+            String passp = request.getParameter("password");
+            Connection con = DatabaseConnection.initializeDatabase();
+
+            PreparedStatement ptm = con.prepareStatement("SELECT * FROM login WHERE username=? AND password=?;");
+            ptm.setString(1, userp);
+            ptm.setString(2, passp);
+            ResultSet rs = ptm.executeQuery();
+            if (rs.next()) {
+                user = rs.getString(1);
+                pass = rs.getString(2);
+            }
+            if (userp.equals(user) && passp.equals(pass)) {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Login Successfully..!');");
+                pw.println("window.location.href = \"UserHome.jsp\";");
+                pw.println("</script>");
+//                RequestDispatcher rd = request.getRequestDispatcher("UserHome.jsp");
+//                rd.forward(request, response);
+            } else {
+                pw.println("<script type=\"text/javascript\">");
+                pw.println("alert('Username or Password is Incorrect..!');");
+                pw.println("window.location.href = \"index.jsp\";");
+                pw.println("</script>");
+//                RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+//                rd.include(request, response);
+            }
+        } catch (Exception e) {
+        }
+    }
+}
